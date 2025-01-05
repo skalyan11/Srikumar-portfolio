ac31fac4df6ece3eafc31672c0e5522a1d46c633
skalyan11 <skalyan@umich.edu>
2025-01-04 18:39:19 -0600

    skills section rough

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css';
 import {NavbarComponent} from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
-
+import { Skills } from './components/Skills';
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
       <Banner />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/img/meter1.svg";
+import colorSharp from "../assets/img/color-sharp.png"; // Import colorSharp
+
+export const Skills = () => {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+
+    return (
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="skill-bx">
+                            <h2>Skills</h2>
+                            <p>Here are some of my skills.</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Design</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Freelancing</h5>
+                                </div>
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} alt="Image" />
+        </section>
+    );
+};
